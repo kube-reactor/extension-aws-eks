95cfb006cfa35238a6b69a9deb8227eb02fe73ae
Adrian Webb <adrian@webb.sh>
2025-02-03 02:30:03 -0500

    Adding a try around the access of ECR repository URLs in the aws-ecr Terraform module outputs.

--- terraform/modules/aws-ecr/outputs.tf
+++ terraform/modules/aws-ecr/outputs.tf
@@ -1,4 +1,4 @@
 output "repository_urls" {
-  value       = aws_ecr_repository.apps[*].repository_url
+  value       = try(aws_ecr_repository.apps[*].repository_url, [])
   description = "The URL of the repositories (in the form aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName)."
 }
