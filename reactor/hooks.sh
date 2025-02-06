ec78a91a8edfa1a2bdb1c197b013ebc19c2b2780
Adrian Webb <adrian@webb.sh>
2025-02-06 18:36:07 -0500

    Removing the depreciated state variables in the reactor hooks.

--- reactor/hooks.sh
+++ reactor/hooks.sh
@@ -1,9 +1,5 @@
 
 function hook_clean_terraform () {
-  rm -f "${__aws_state_project_dir}/.terraform.lock.hcl"
-  rm -f "${__aws_state_project_dir}/terraform.tfvars"
-  rm -f "${__aws_state_project_dir}/terraform.tfstate"*
-
   rm -f "${__aws_infrastructure_project_dir}/.terraform.lock.hcl"
   rm -f "${__aws_cluster_project_dir}/.terraform.lock.hcl"
   rm -f "${__aws_applications_project_dir}/.terraform.lock.hcl"
