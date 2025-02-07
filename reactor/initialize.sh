c3bf14a3e3878f8b494af858be671ff7b6e40a4f
Adrian Webb <adrian@webb.sh>
2025-02-07 00:06:58 -0500

    Adding a provider conditional around extension initialization environment in the reactor initialize script.

--- reactor/initialize.sh
+++ reactor/initialize.sh
@@ -11,52 +11,53 @@ export __aws_eks_terraform_dir="${__aws_eks_extension_dir}/terraform"
 export __aws_infrastructure_project_dir="${__aws_eks_terraform_dir}/infrastructure"
 export __aws_cluster_project_dir="${__aws_eks_terraform_dir}/kubernetes-cluster"
 export __aws_applications_project_dir="${__aws_eks_terraform_dir}/applications"
-#
-# AWS
-#
-export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-}"
-export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-}"
 
 if [ "$KUBERNETES_PROVIDER" == "aws_eks" ]; then
+  #
+  # AWS
+  #
+  export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-}"
+  export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-}"
+
   if [[ ! "$AWS_ACCESS_KEY_ID" ]] || [[ ! "$AWS_SECRET_ACCESS_KEY" ]]; then
     emergency "To provision AWS resources, you must specify AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables"
   fi
+
+  export AWS_EKS_REGION="${AWS_EKS_REGION:-"us-east-1"}"
+  #
+  # Networking
+  #
+  export TF_VAR_region="$AWS_EKS_REGION"
+  export TF_VAR_vpc_name="$APP_NAME"
+
+  export TF_VAR_vpc_cidr="$AWS_VPC_CIDR"
+  export TF_VAR_private_subnet_cidrs="$AWS_VPC_PRIVATE_SUBNET_CIDRS"
+  export TF_VAR_public_subnet_cidrs="$AWS_VPC_PUBLIC_SUBNET_CIDRS"
+  #
+  # Container Registry
+  #
+  export TF_VAR_repositories="${AWS_ECR_REPOSITORIES:-"\{\}"}"
+  export TF_VAR_force_delete="${AWS_ECR_FORCE_DELETE:-false}"
+  export TF_VAR_image_tag_mutability="${AWS_ECR_IMAGE_TAG_MUTABILITY:-MUTABLE}"
+  export TF_VAR_scan_images_on_push="${AWS_ECR_SCAN_IMAGES_ON_PUSH:-false}"
+  #
+  # Kubernetes Cluster
+  #
+  export TF_VAR_eks_name="$APP_NAME"
+
+  export TF_VAR_eks_system_node_instance_type="$AWS_SYSTEM_NODE_TYPE"
+  export TF_VAR_eks_min_system_nodes="$AWS_SYSTEM_NODE_MIN_COUNT"
+  export TF_VAR_eks_max_system_nodes="$AWS_SYSTEM_NODE_MAX_COUNT"
+
+  export TF_VAR_eks_ops_node_instance_type="$AWS_OPS_NODE_TYPE"
+  export TF_VAR_eks_min_ops_nodes="$AWS_OPS_NODE_MIN_COUNT"
+  export TF_VAR_eks_max_ops_nodes="$AWS_OPS_NODE_MAX_COUNT"
+
+  export TF_VAR_eks_app_node_instance_type="$AWS_APP_NODE_TYPE"
+  export TF_VAR_eks_min_app_nodes="$AWS_APP_NODE_MIN_COUNT"
+  export TF_VAR_eks_max_app_nodes="$AWS_APP_NODE_MAX_COUNT"
+  #
+  # Load Balancer
+  #
+  export TF_VAR_domain="$PRIMARY_DOMAIN"
 fi
-
-export AWS_EKS_REGION="${AWS_EKS_REGION:-"us-east-1"}"
-#
-# Networking
-#
-export TF_VAR_region="$AWS_EKS_REGION"
-export TF_VAR_vpc_name="$APP_NAME"
-
-export TF_VAR_vpc_cidr="$AWS_VPC_CIDR"
-export TF_VAR_private_subnet_cidrs="$AWS_VPC_PRIVATE_SUBNET_CIDRS"
-export TF_VAR_public_subnet_cidrs="$AWS_VPC_PUBLIC_SUBNET_CIDRS"
-#
-# Container Registry
-#
-export TF_VAR_repositories="${AWS_ECR_REPOSITORIES:-"\{\}"}"
-export TF_VAR_force_delete="${AWS_ECR_FORCE_DELETE:-false}"
-export TF_VAR_image_tag_mutability="${AWS_ECR_IMAGE_TAG_MUTABILITY:-MUTABLE}"
-export TF_VAR_scan_images_on_push="${AWS_ECR_SCAN_IMAGES_ON_PUSH:-false}"
-#
-# Kubernetes Cluster
-#
-export TF_VAR_eks_name="$APP_NAME"
-
-export TF_VAR_eks_system_node_instance_type="$AWS_SYSTEM_NODE_TYPE"
-export TF_VAR_eks_min_system_nodes="$AWS_SYSTEM_NODE_MIN_COUNT"
-export TF_VAR_eks_max_system_nodes="$AWS_SYSTEM_NODE_MAX_COUNT"
-
-export TF_VAR_eks_ops_node_instance_type="$AWS_OPS_NODE_TYPE"
-export TF_VAR_eks_min_ops_nodes="$AWS_OPS_NODE_MIN_COUNT"
-export TF_VAR_eks_max_ops_nodes="$AWS_OPS_NODE_MAX_COUNT"
-
-export TF_VAR_eks_app_node_instance_type="$AWS_APP_NODE_TYPE"
-export TF_VAR_eks_min_app_nodes="$AWS_APP_NODE_MIN_COUNT"
-export TF_VAR_eks_max_app_nodes="$AWS_APP_NODE_MAX_COUNT"
-#
-# Load Balancer
-#
-export TF_VAR_domain="$PRIMARY_DOMAIN"
