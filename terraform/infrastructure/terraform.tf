44221b83fa713623cb712ecbbcaab6596486463b
Adrian Webb <adrian@webb.sh>
2025-05-21 14:01:31 -0400

    Updating the dependency versions in the infrastructure Terraform module.

--- terraform/infrastructure/terraform.tf
+++ terraform/infrastructure/terraform.tf
@@ -1,25 +1,25 @@
 terraform {
-  required_version = "~> 1.3"
+  required_version = ">= 1.12.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.84.0"
+      version = "~> 5.98.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6.1"
+      version = "~> 3.7.2"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.5"
+      version = "~> 4.1.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.4"
+      version = "~> 2.3.7"
     }
   }
 }
